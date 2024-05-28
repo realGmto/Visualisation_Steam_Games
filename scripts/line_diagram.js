336494861519db84a69d1834a703794f3a087740
Nikola Muženjak <nmuzenjak@etfos.hr>
2024-05-28 17:34:16 +0200

    Update line_diagram

--- scripts/line_diagram.js
+++ scripts/line_diagram.js
@@ -8,8 +8,8 @@ const innerHeight_line = height_line - margin_line.top - margin_line.bottom;
 const svg_line = d3.select("#line_diagram").append("g")
     .attr("transform", `translate(${margin_line.left},${margin_line.top})`);
 
-let x_line;
-let y_line;
+const x_line = d3.scaleTime().range([0, innerWidth_line]);
+const y_line = d3.scaleLinear().range([innerHeight_line, 0]);
 
 const xAxis_line = svg_line.append("g")
             .attr("class", "x-axis")
@@ -25,16 +25,10 @@ let line;
 
 
 function Draw_line_diagram(){
-    x_line = d3.scaleTime()
-        .domain(d3.extent(data_line, d => d.time))
-        .range([0, innerWidth_line]);
+    x_line.domain(d3.extent(data_line, d => d.time));
+    y_line.domain([0, Math.max(...data_line.map(o => o.price))]).nice();   //DO NOT USE d3.max() for some reason it doesn't return correctly. ONLY ON THIS!
 
-    y_line = d3.scaleLinear()
-        .domain([0, Math.max(...data_line.map(o => o.price))])   //DO NOT USE d3.max() for some reason it doesn't return correctly. ONLY ON THIS!
-        .nice()
-        .range([innerHeight_line, 0]);
-
-    xAxis_line.transition().duration(750).call(d3.axisBottom(x_line));
+    xAxis_line.transition().duration(750).call(d3.axisBottom(x_line).ticks().tickFormat(d3.format(".0f")));
     yAxis_line.transition().duration(750).call(d3.axisLeft(y_line).tickSize(0).tickPadding(10))
 
     line = d3.line()
@@ -95,15 +89,15 @@ function Draw_line_diagram(){
     // Add labels for the axes
     svg_line.append("text")
         .attr("class", "x-label")
-        .attr("x", width_line / 2 + margin_line.left)
-        .attr("y", height_line - 10)
+        .attr("x", innerWidth_line / 2)
+        .attr("y", height_line+margin_line.top)
         .text("Year");
 
     svg_line.append("text")
         .attr("class", "y-label")
         .attr("text-anchor", "middle")
         .attr("x", -height_line / 2)
-        .attr("y", -22)
+        .attr("y", -25)
         .attr("transform", "rotate(-90)")
         .text("Average Price");
 }
@@ -112,28 +106,31 @@ function Draw_line_diagram(){
 function update_line_diagram() {
     // Update scales
     x_line.domain(d3.extent(data_line, d => d.time));
-    y_line.domain([0, Math.max(...data_line.map(o => o.price))]);
+    y_line.domain([0, d3.max(data_line, d => parseFloat(d.price))]).nice();
 
-    const svg = d3.select("#line_diagram");
+    console.log(d3.max(data_line, d => parseFloat(d.price)));
+
+    data_line.forEach( d => console.log(d.time+" = "+y_line(d.price) + ', ' + x_line(d.time)));
+
 
     // Update axes
-    xAxis_line.transition().duration(750).call(d3.axisBottom(x_line));
-    yAxis_line.transition().duration(750).call(d3.axisLeft(y_line).tickSize(0).tickPadding(10));
+    xAxis_line.transition().duration(750).call(d3.axisBottom(x_line).ticks().tickFormat(d3.format(".0f")));
+    yAxis_line.transition().duration(750).call(d3.axisLeft(y_line).tickSize(5).tickPadding(10));
 
     // Define the line
-    const line = d3.line()
+    line = d3.line()
         .defined(d => !isNaN(d.price))
         .x(d => x_line(d.time))
         .y(d => y_line(d.price));
 
     // Update line path
-    svg.select(".line")
+    svg_line.select(".line")
         .datum(data_line)
         .transition().duration(750)
         .attr("d", line);
 
     // Update circles
-    const circles = svg.selectAll(".dot")
+    const circles = svg_line.selectAll(".dot")
         .data(data_line);
 
     circles.enter().append("circle")
